af9b982c1f12bd60ea21864f797dcea8a43d4a8d
khalek-repliq <abdul.khalek@repliq.dev>
2025-04-22 00:31:25 +0600

    Fixed suspense issue

--- app/(student)/dashboard/layout.jsx
+++ app/(student)/dashboard/layout.jsx
@@ -1,17 +1,19 @@
 import BaseFooter from "@/components/BaseFooter";
 import BaseNavbar from "@/components/navbars/BaseNavbar";
 import StudentAuthGuardHOC from "@/components/StudentAuthGuardHOC";
-import React from "react";
+import React, { Suspense } from "react";
 
 const Dashboard = ({ children }) => {
   return (
-    <div>
-      <StudentAuthGuardHOC>
-        <BaseNavbar />
-        <div className="min-h-screen">{children}</div>
-        <BaseFooter />
-      </StudentAuthGuardHOC>
-    </div>
+    <Suspense>
+      <div>
+        <StudentAuthGuardHOC>
+          <BaseNavbar />
+          <div className="min-h-screen">{children}</div>
+          <BaseFooter />
+        </StudentAuthGuardHOC>
+      </div>
+    </Suspense>
   );
 };
 
